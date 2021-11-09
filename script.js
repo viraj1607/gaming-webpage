80f23a46b055c8817860678112dc8242ccec81ff
vir16 <83710312+vir16@users.noreply.github.com>
2021-11-09 15:57:18 +0530

    AOS

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeRight(){
+    let sec=document.getElementById("gaming-images");
+    sec.style.background="url(./images/bg2.jpg)";
+}
+
+function changeLeft(){
+    let sec=document.getElementById("gaming-images");
+    sec.style.background="url(./images/slider-1.jpg.webp)";
+}
